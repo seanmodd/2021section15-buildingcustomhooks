0f29947221883d1f7cedccba3719f6529198d123
Sean <sean@vestaridefair.com>
2021-08-01 20:15:01 -0700

    about to finish section 15 on custom hooks, this was super hard

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import {
   Button,
   Heading,
@@ -7,6 +7,10 @@ import {
 } from '@chakra-ui/react';
 import BackwardCounter from '../components/BackwardCounter';
 import ForwardCounter from '../components/ForwardCounter';
+import NewTask from '../components/NewTask/NewTask';
+import Tasks from '../components/Tasks/Tasks';
+// import useHttp from '.././hooks/useHttp';
+import useHttp from '../hooks/use-http';
 
 
 const Index = () => {
@@ -21,7 +25,28 @@ const Index = () => {
     dark: 'black',
   };
 //!
+          const [tasks, setTasks] = useState([]);
   
+  const {isLoading, error, sendRequest: fetchTasks} = useHttp();
+
+useEffect(() => {
+  const transformTasks = (tasksObj) => {
+    const loadedTasks = [];
+
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+    }
+
+    setTasks(loadedTasks)
+  };
+  fetchTasks(
+    {url: 'https://react-http-movies-post-default-rtdb.firebaseio.com/tasks.json'}, 
+    transformTasks);
+   }, [fetchTasks]);
+
+const taskAddHandler = (task) => {
+  setTasks((prevTasks) => prevTasks.concat(task));
+};
   return (
     <>
       <VStack minHeight="100vh" bg={bgColor[colorMode]}>
@@ -36,6 +61,14 @@ const Index = () => {
           <BackwardCounter />
           <ForwardCounter />
         </VStack>
+        <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+      
       </VStack>
     </>
   );
